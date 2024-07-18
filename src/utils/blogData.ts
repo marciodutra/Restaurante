8c118852c7cdef40f694eb24f005314ec06203e7
Márcio de Oliveira Dutra <78557074+marciodutra@users.noreply.github.com>
2024-07-18 15:18:38 -0300

    Add files via upload

---
+++ src/utils/blogData.ts
@@ -0,0 +1,52 @@
+const blogData: {
+  id: number;
+  title: string;
+  description: string;
+  time: string;
+  img: string;
+}[] = [
+  {
+    id: 1,
+    title: "Gatsby Night",
+    description:
+      "Faça seu evento no Gatsby Night - Os melhores drinks e chopes internacionais, gastronomia incrível e eventos no Gatsby Night!",
+    time: "19 de setembro de 2024",
+    img: "https://images.unsplash.com/photo-1642773520998-7bc84e6afd80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1176&q=80",
+  },
+
+  {
+    id: 2,
+    title: "Show de mágica",
+    description:
+      "Uma experiência inesquecível para seu evento com um dos maiores ilusionistas do Brasil",
+    time: "19 de setembro de 2024",
+    img: "https://plus.unsplash.com/premium_photo-1663011391833-e575d20b3a88?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+  },
+
+  {
+    id: 3,
+    title: "Novo restaurante",
+    description:
+      "Além do novo endereço, a marca busca, agora, ser uma churrascaria mais sofisticada, com modificações no ambiente, no cardápio e no atendimento",
+    time: "19 de setembro de 2024",
+    img: "https://images.unsplash.com/photo-1649695121711-2f2ea8e8faf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    id: 4,
+    title: "Jantar romântico",
+    description:
+      " Explore a pasta Jantar romântico de Cassio Rodrigo no Pinterest. Veja mais ideias sobre jantar romântico.",
+    time: "19 de setembro de 2024",
+    img: "https://images.unsplash.com/photo-1525818080574-aa8f3221fdee?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80",
+  },
+  {
+    id: 5,
+    title: "Cozinha totalmente nova",
+    description:
+      "Ótimo restaurante com buffet diversificado, muitas saladas, pratos quentes, várias carnes e um buffet de sobremesa deliciosa!",
+    time: "September 29, 2022",
+    img: "https://images.unsplash.com/photo-1580929211634-0e8f1adae279?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80",
+  },
+];
+
+export default blogData;
